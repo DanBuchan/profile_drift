54160c18afa965da0c181f581f59d68f38e55048
Daniel Buchan <clone@myself.com>
2022-03-18 12:25:38 +0000

    updated generation to track what step/distance it is at for each sequence. Removed final_array accumulation to reduce memory use at the very end. Changed output_strings to print the distance in the fasta headers. Sadly a no longer efficient use of where

--- sequence_generator.py
+++ sequence_generator.py
@@ -18,12 +18,16 @@ def read_seq(file):
 
 
 def output_strings(file, proteins):
-    '''Output a fasta file of the string we generated'''
+    '''Output a fasta file of the strings we generated'''
     string_count = 0
     with open(file, "w", encoding="utf-8") as fh_out:
         for protein in proteins:
-            fh_out.write(f'>{string_count}\n')
-            fh_out.write(f'{"".join(protein)}\n')
+            working_string = np.array(protein[0], dtype='U13')
+            for y in range(alphabet_size):
+                working_string = np.where(working_string == str(y), alph[y],
+                                          working_string)
+            fh_out.write(f'>{string_count}; approx_dist: {protein[1]}\n')
+            fh_out.write(f'{"".join(working_string)}\n')
             string_count += 1
 
 
@@ -39,14 +43,11 @@ parser = argparse.ArgumentParser(description='Generate a sets of (approx)'
                                  'equi-spaced proteins',
                                  prog='sequence_generator.py',
                                  usage='\n%(prog)s --num_strings INT '
-                                 '--alphabet_size INT '
                                  '--distance INT '
                                  '--output_file STRING '
                                  '--starting_string_file STRING')
 parser.add_argument('--num_strings', help="Number of protein string to "
                     "generate", required=True, type=check_positive)
-parser.add_argument('--alphabet_size', help="Size of...", required=True,
-                    type=check_positive)
 parser.add_argument('--distance', help="distance apart for each generated "
                     "string", required=True, type=check_positive)
 parser.add_argument('--output_file', help="Name of output file", required=True)
@@ -56,10 +57,10 @@ args = parser.parse_args()
 
 strings = []
 hashed = []
-
 alph = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q',
         'R', 'S', 'T', 'V', 'W', 'Y']
 
+alphabet_size = len(alph)
 curr_string = read_seq(args.starting_string_file)
 length_of_strings = len(curr_string)
 count = 0
@@ -68,28 +69,26 @@ for char in alph:
     curr_string[curr_string == char] = int(count)
     count = count + 1
 curr_string = curr_string.astype('int32')
-
+accumulated_dist = 0
+# here we keep track of how many steps/moves we're making at each iteration
+# so we can report that in the fasta header on output
 while True:
     for x in range(args.num_strings):
+        accumulated_dist += args.distance
         pos_to_change = random.sample(range(0, length_of_strings),
                                       args.distance)
-        replacements = random.sample(range(0, args.alphabet_size),
+        replacements = random.sample(range(0, alphabet_size),
                                      args.distance)
-
         while sum(curr_string[pos_to_change] == replacements) > 0:
-            replacements = random.sample(range(0, args.alphabet_size),
+            replacements = random.sample(range(0, alphabet_size),
                                          args.distance)
 
         curr_string[pos_to_change] = replacements
-        strings.append(list(curr_string))
+        strings.append([list(curr_string), accumulated_dist])
         hashed.append(curr_string.tobytes())
 
     unique = set(hashed)
     if len(unique) == len(strings):
         break
 
-final_array = np.array(strings, dtype='U13')
-for x in range(args.alphabet_size):
-    final_array = np.where(final_array == str(x), alph[x], final_array)
-
-output_strings(args.output_file, final_array)
+output_strings(args.output_file, strings)
