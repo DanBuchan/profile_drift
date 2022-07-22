5ac247a20a0c179bbe95a410e72af918fb5e18e5
Daniel Buchan <clone@myself.com>
2022-07-22 17:59:14 +0100

    enable all experiment"

--- drift_experiment.sh
+++ drift_experiment.sh
@@ -11,11 +11,11 @@ echo "generating sets with familes"
 # python /home/dbuchan/Projects/profile_drift/create_family.py distance50.fa distance50
 
 echo "run distance experiments"
-# for ste in '2' '5' '10' '20' '30' '40' '50';
-for step in '2';
+for ste in '2' '5' '10' '20' '30' '40' '50';
+#for step in '2';
 do
-  #for id in '1000' '1500' '2000' '2500' '3000' '3500' '4000' '4500' '5000' '55000' '6000' '6500' '7000' '7500' '8000' '8500' '9000' '9500';
-  for id in '1000';
+  for id in '1000' '1500' '2000' '2500' '3000' '3500' '4000' '4500' '5000' '55000' '6000' '6500' '7000' '7500' '8000' '8500' '9000' '9500';
+  #for id in '1000';
   do
     db="distance"$step"_500cluster_"$id"cluster.fa"
     seed="distance"$step"_500_seed.fa"
@@ -25,5 +25,7 @@ do
     dist="distance"$step"_500cluster_"$id"cluster.dist"
     echo "/home/dbuchan/Applications/standard-RAxML/raxmlHPC-PTHREADS-AVX -T 6 -s $db -n $dist -m PROTGAMMABLOSUM62 -N2 -p 123 -f x > stdout 2> stderr"
     echo "python /home/dbuchan/Projects/profile_drift/run_blasts.py $db $seed 20 $membership $raxml $averages"
+    /home/dbuchan/Applications/standard-RAxML/raxmlHPC-PTHREADS-AVX -T 6 -s $db -n $dist -m PROTGAMMABLOSUM62 -N2 -p 123 -f x > stdout 2> stderr
+    python /home/dbuchan/Projects/profile_drift/run_blasts.py $db $seed 20 $membership $raxml $averages
   done
 done
