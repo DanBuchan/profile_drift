aee1207d17ca9e3b540fa77a41bcde1a71c9ad74
Daniel Buchan <clone@myself.com>
2022-07-22 18:05:58 +0100

    typo fixed

--- drift_experiment.sh
+++ drift_experiment.sh
@@ -2,19 +2,19 @@
 
 # generate sequence dbs
 echo "generating sets with familes"
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance2.fa distance2
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance5.fa distance5
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance10.fa distance10
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance20.fa distance20
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance30.fa distance30
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance40.fa distance40
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance50.fa distance50
+python /home/dbuchan/Projects/profile_drift/create_family.py distance2.fa distance2
+python /home/dbuchan/Projects/profile_drift/create_family.py distance5.fa distance5
+python /home/dbuchan/Projects/profile_drift/create_family.py distance10.fa distance10
+python /home/dbuchan/Projects/profile_drift/create_family.py distance20.fa distance20
+python /home/dbuchan/Projects/profile_drift/create_family.py distance30.fa distance30
+python /home/dbuchan/Projects/profile_drift/create_family.py distance40.fa distance40
+python /home/dbuchan/Projects/profile_drift/create_family.py distance50.fa distance50
 
 echo "run distance experiments"
 for ste in '2' '5' '10' '20' '30' '40' '50';
 #for step in '2';
 do
-  for id in '1000' '1500' '2000' '2500' '3000' '3500' '4000' '4500' '5000' '55000' '6000' '6500' '7000' '7500' '8000' '8500' '9000' '9500';
+  for id in '1000' '1500' '2000' '2500' '3000' '3500' '4000' '4500' '5000' '5500' '6000' '6500' '7000' '7500' '8000' '8500' '9000' '9500';
   #for id in '1000';
   do
     db="distance"$step"_500cluster_"$id"cluster.fa"
