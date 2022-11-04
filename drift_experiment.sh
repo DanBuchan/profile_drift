46784db82a80ccb9ce1bb01c0809a57d9bb93712
Daniel Buchan <clone@myself.com>
2022-11-04 11:50:33 +0000

    updated small bits

--- drift_experiment.sh
+++ drift_experiment.sh
@@ -2,18 +2,16 @@
 
 # generate sequence dbs
 echo "generating sets with familes"
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance2.fa distance2_membership.csv distance2
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance5.fa distance5_membership.csv distance5
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance10.fa distance10_membership.csv distance10
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance20.fa distance20_membership.csv distance20
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance30.fa distance30_membership.csv distance30
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance40.fa distance40_membership.csv distance40
-# python /home/dbuchan/Projects/profile_drift/create_family.py distance50.fa distance50_membership.csv distance50
+python /home/dbuchan/Projects/profile_drift/create_family.py distance2.fa distance2_membership.csv distance2
+python /home/dbuchan/Projects/profile_drift/create_family.py distance5.fa distance5_membership.csv distance5
+python /home/dbuchan/Projects/profile_drift/create_family.py distance10.fa distance10_membership.csv distance10
+python /home/dbuchan/Projects/profile_drift/create_family.py distance20.fa distance20_membership.csv distance20
+python /home/dbuchan/Projects/profile_drift/create_family.py distance30.fa distance30_membership.csv distance30
+python /home/dbuchan/Projects/profile_drift/create_family.py distance40.fa distance40_membership.csv distance40
+python /home/dbuchan/Projects/profile_drift/create_family.py distance50.fa distance50_membership.csv distance50
 
 echo "run distance experiments"
-# for step in '2' '5' '10' '20' '30' '40' '50';
-# for step in '2';
-for step in '10' '20';
+for step in '2' '5' '10' '20' '30' '40' '50';
 do
   for entry in "./distance"$step"_"*"cluster_"*"cluster.fa";
   # for id in '1000';
@@ -29,9 +27,9 @@ do
     blastdir="distance"$step"_"$start"cluster_"$id"cluster"
     echo "/home/dbuchan/Applications/standard-RAxML/raxmlHPC-PTHREADS-AVX -T 6 -s $db -n $dist -m PROTGAMMABLOSUM62 -N2 -p 123 -f x > stdout 2> stderr"
     echo "python /home/dbuchan/Projects/profile_drift/run_blasts.py $db $seed 20 $membership $raxml $averages"
-    # /home/dbuchan/Applications/standard-RAxML/raxmlHPC-PTHREADS-AVX -T 6 -s $db -n $dist -m PROTGAMMABLOSUM62 -N2 -p 123 -f x > stdout 2> stderr
+    /home/dbuchan/Applications/standard-RAxML/raxmlHPC-PTHREADS-AVX -T 6 -s $db -n $dist -m PROTGAMMABLOSUM62 -N2 -p 123 -f x > stdout 2> stderr
     python /home/dbuchan/Projects/profile_drift/run_blasts.py $db $seed 20 $membership $raxml $averages
-    # mkdir $blastdir
+    mkdir $blastdir
     mv iteration*.bls $blastdir
   done
 done
