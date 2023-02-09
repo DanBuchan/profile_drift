e3e8eb3575b7375e4c7fc3e24ba27719b878af65
Daniel Buchan <clone@myself.com>
2023-02-09 17:50:45 +0000

    more tidy files

--- run_pfam.sh
+++ run_pfam.sh
@@ -25,5 +25,7 @@
 # Run the application.
 module load python3
 source /home/ucbcdwb/Scratch/profile_drift/profile_drift/bin/activate
+mkdir $SGE_TASK_ID
+cd $SGE_TASK_ID/
 echo "python run_pfam_rep_blasts.py ~/Scratch/Data/pfam/reps.fasta.fa ~/Scratch/Data/pfam/pfam_fasta.fa $SGE_TASK_ID"
 python /home/ucbcdwb/Scratch/profile_drift/run_pfam_rep_blasts.py ~/Scratch/Data/pfam/reps.fasta.fa ~/Scratch/Data/pfam/pfam_fasta.fa $SGE_TASK_ID
