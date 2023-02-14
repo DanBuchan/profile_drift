d8ab3ecda47289e6e6c8b7dd61a5b0cd08da0397
Daniel Buchan <clone@myself.com>
2023-02-14 13:08:58 +0000

    blast parser

--- parse_pfam_blasts.py
+++ parse_pfam_blasts.py
@@ -10,3 +10,26 @@ subfolders = [ f.path for f in os.scandir(results_path) if f.is_dir() ]
 for dir in subfolders:
     for file in glob.glob(f"{dir}/*.bls"):
         print(file)
+        count+=1
+        read = False
+        with open(file, "r") as fh:
+            for line in fh:
+                if line.startswith('Sequences producing significant'):
+                     read = True
+                     continue
+                if line.startswith('>'):
+                    read = False
+                    continue
+                if read:
+                    result = re.match(result_list_pattern, line)
+                    if result:
+                        if float(result.groups()[2]) < 1e-5:
+                            if result.groups()[0] not in seen:
+                                results[count].append(result.groups()[0])
+                                seen.append(result.groups()[0])
+                            #print(result.groups()[0])
+
+        for iteration in results.keys():
+        for hit in results[iteration]:
+            pass
+            #print(f'{iteration},{hit}')
