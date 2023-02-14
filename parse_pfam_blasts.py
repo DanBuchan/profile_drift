fd6575fdc624fda63f1afae9d807a0e1aee83acb
Daniel Buchan <clone@myself.com>
2023-02-14 12:44:36 +0000

    blast parser

--- parse_pfam_blasts.py
+++ parse_pfam_blasts.py
@@ -1,9 +1,12 @@
 import glob
 import sys
+import os
 import re
 from collections import defaultdict
 
 results_path = sys.argv[1]
 
-for file in glob.glob("*.bls"):
-    print(file)
+for dir in os.walk(results_path):
+    print(dir)
+    for file in glob.glob(f"{dir}*.bls"):
+        print(file)
