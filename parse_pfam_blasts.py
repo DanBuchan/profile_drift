da59c988ae5803150d610ed36bc17611933ef619
Daniel Buchan <clone@myself.com>
2023-02-14 12:50:53 +0000

    blast parser

--- parse_pfam_blasts.py
+++ parse_pfam_blasts.py
@@ -8,5 +8,5 @@ results_path = sys.argv[1]
 
 subfolders = [ f.path for f in os.scandir(results_path) if f.is_dir() ]
 for dir in subfolders:
-    for file in glob.glob(f"{dir}/*.bls")
+    for file in glob.glob(f"{dir}/*.bls"):
         print(file)
