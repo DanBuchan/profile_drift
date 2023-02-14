34395257359eaca7e404e004f53884a7778efe43
Daniel Buchan <clone@myself.com>
2023-02-14 12:47:14 +0000

    blast parser

--- parse_pfam_blasts.py
+++ parse_pfam_blasts.py
@@ -8,5 +8,5 @@ results_path = sys.argv[1]
 
 for path_data in os.walk(results_path):
     for dir in path_data[1]:
-        for file in glob.glob(f"{dir}*.bls"):
+        for file in glob.glob(f"{dir}/*.bls"):
             print(file)
