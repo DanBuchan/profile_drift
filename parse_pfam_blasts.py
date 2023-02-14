ef1c5d8bc32d35c5bb87174750628aedaf9641c9
Daniel Buchan <clone@myself.com>
2023-02-14 12:46:26 +0000

    blast parser

--- parse_pfam_blasts.py
+++ parse_pfam_blasts.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 
 results_path = sys.argv[1]
 
-for dir in os.walk(results_path):
-    print(dir)
-    for file in glob.glob(f"{dir}*.bls"):
-        print(file)
+for path_data in os.walk(results_path):
+    for dir in path_data[1]
+        for file in glob.glob(f"{dir}*.bls"):
+            print(file)
