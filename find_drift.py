14b0bf0141a152ed77fa620fdb09e1c410de3fdb
Daniel Buchan <clone@myself.com>
2023-02-15 15:36:12 +0000

    pfam data hnandling

--- find_drift.py
+++ find_drift.py
@@ -4,8 +4,16 @@ from collections import defaultdict
 
 
 def process_data(data):
+    seen = []
+    summary = {}
     for iteration in sorted(data):
-        print(iteration)
+        counts = defaultdict(int)
+        for data_row in iteration:
+            if now data_row[2] in seen:
+                seen.append(data_row[2])
+                counts[data[3]] += 1
+        summary[iteration] = counts
+    print(summary)
 
 blast_summary_file = sys.argv[1]
 current_query = None
