6cc049caaf6d57aae7869f7857b289decc222039
Daniel Buchan <clone@myself.com>
2023-02-15 14:27:17 +0000

    pfam data hnandling

--- find_drift.py
+++ find_drift.py
@@ -5,7 +5,7 @@ blast_summary_file = sys.argv[1]
 current_query = None
 query_count = 0
 with open(blast_summary_file, 'r') as datafile:
-    blast_reader = csv.reader(csvfile, delimiter=',',)
+    blast_reader = csv.reader(datafile, delimiter=',',)
     next(blast_reader)
     for row in blast_reader:
         print(row)
