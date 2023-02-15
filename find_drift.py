a6d912ea339cdddb506e377b1cc4ed69bcb97cc7
Daniel Buchan <clone@myself.com>
2023-02-15 15:08:05 +0000

    pfam data hnandling

--- find_drift.py
+++ find_drift.py
@@ -12,14 +12,13 @@ with open(blast_summary_file, 'r') as datafile:
     next(blast_reader)
 
     for row in blast_reader:
-        if not current_query:
-            current_query = row[0]
-        elif (not current_query) and not row[0] == current_query:
+        if (not current_query) and not row[0] == current_query:
             #process prior data
             count+=1
             current_query = row[0]
             print(data)
             exit()
+        current_query = row[0]
         data[int(row[1])] = row
 
 print(f"Processed pfam families: {count}")
