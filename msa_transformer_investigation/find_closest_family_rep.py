8798260face892cab624156ef95befd38ab5d987
Daniel Buchan <clone@myself.com>
2023-03-28 17:13:03 +0100

    update

--- msa_transformer_investigation/find_closest_family_rep.py
+++ msa_transformer_investigation/find_closest_family_rep.py
@@ -47,7 +47,7 @@ def parse_pfam_alignments(pfam_aligns, drift_families):
 
     with open("famiies_list.txt") as fhOut:
         for entry in nr_list:
-            fhOut.write(f'{entry}}\n')
+            fhOut.write(f'{entry}\n')
         
 
 
