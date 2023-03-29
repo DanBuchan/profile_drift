8f36abf0026479bbaf3445c48a9e239692d811f8
Daniel Buchan <clone@myself.com>
2023-03-29 13:50:31 +0100

    this

--- msa_transformer_investigation/find_closest_family_rep.py
+++ msa_transformer_investigation/find_closest_family_rep.py
@@ -14,42 +14,43 @@ def parse_pfam_alignments(pfam_aligns, drift_families):
              nr_list.add(item)
 
     align_count = 0
-    with open(pfam_aligns, "r") as fh:
+    with open(pfam_aligns, "rb") as fh:
         align_name = ''
         msa = defaultdict(list)
-        try:
-            for line in fh:
-                if line.startswith("//"):
-                    continue
-                if line.startswith("# STOCKHOLM"):
-                    if align_count != 0:
-                        if align_name in nr_list:
-                            print(f"Printing: {align_name}")
-                            with open(f"{align_name}.fa", "w") as fhOut:
-                                for msa_line in msa[align_name]:
-                                    fhOut.write(f">{msa_line[0]}\n")
-                                    fhOut.write(f"{msa_line[1]}\n")             
-                    else:
-                        align_count+=1
-                    align_name = ''
-                    msa = defaultdict(list)
-                if line.startswith("#=GF AC   "):
-                    align_name = line[10:].rstrip()
-                    align_name = align_name.split(".", 1)[0]
-                if not line.startswith("#"):
-                    entries = line.split()
-                    seq = entries[1].replace('-', '')
-                    seq = seq.replace('.', '')
-                    seq_data = (entries[0], seq)
-                    msa[align_name].append(seq_data)
-            print(f"Printing: {align_name}")
-            with open(f"{align_name}.fa") as fhOut:
-                for msa_line in msa[align_name]:
-                    fhOut.write(f">{msa_line[0]}\n")
-                    fhOut.write(f"{msa_line[1]}\n")
-        except Exception as e:
-            print(e)
-            exit()
+        for line_binary in fh:
+            try:
+                line = line_binary.decode("utf-8")
+            except Exception as e:
+                print(e)
+                exit()
+            if line.startswith("//"):
+                continue
+            if line.startswith("# STOCKHOLM"):
+                if align_count != 0:
+                    if align_name in nr_list:
+                        print(f"Printing: {align_name}")
+                        with open(f"{align_name}.fa", "w") as fhOut:
+                            for msa_line in msa[align_name]:
+                                fhOut.write(f">{msa_line[0]}\n")
+                                fhOut.write(f"{msa_line[1]}\n")             
+                else:
+                    align_count+=1
+                align_name = ''
+                msa = defaultdict(list)
+            if line.startswith("#=GF AC   "):
+                align_name = line[10:].rstrip()
+                align_name = align_name.split(".", 1)[0]
+            if not line.startswith("#"):
+                entries = line.split()
+                seq = entries[1].replace('-', '')
+                seq = seq.replace('.', '')
+                seq_data = (entries[0], seq)
+                msa[align_name].append(seq_data)
+        print(f"Printing: {align_name}")
+        with open(f"{align_name}.fa") as fhOut:
+            for msa_line in msa[align_name]:
+                fhOut.write(f">{msa_line[0]}\n")
+                fhOut.write(f"{msa_line[1]}\n")
 
     with open("famiies_list.txt") as fhOut:
         for entry in nr_list:
