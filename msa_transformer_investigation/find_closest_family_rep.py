43c62ca68e7c00a8dd0d5392b732e3f2bb8d5c38
Daniel Buchan <clone@myself.com>
2023-04-03 18:22:08 +0100

    this

--- msa_transformer_investigation/find_closest_family_rep.py
+++ msa_transformer_investigation/find_closest_family_rep.py
@@ -122,7 +122,7 @@ def run_fasta(seq, target_family):
     best_score = 100000000
     for line in lines:
         if parse_results:
-            score = line[68:].replace(" ", "")
+            score = float(line[68:].replace(" ", ""))
             print(score)
         if line.startswith("The best scores are:"):
             parse_results = True
