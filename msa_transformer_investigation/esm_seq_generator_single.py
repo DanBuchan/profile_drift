6b1d2925c4acff7447542e6b1f4b0a8ab6b214e2
Daniel Buchan <clone@myself.com>
2023-04-24 17:38:25 +0100

    stuff

--- msa_transformer_investigation/esm_seq_generator_single.py
+++ msa_transformer_investigation/esm_seq_generator_single.py
@@ -195,8 +195,7 @@ def generate_seqs(msa, transformer, transformer_alphabet, align_name, mask_amoun
         # exit()
         transformer_batch_labels, transformer_batch_strs, transformer_batch_tokens = transformer_batch_converter([inputs])
         input_tokens = transformer_batch_tokens.cpu().numpy()[0]
-        print(input_tokens.size)
-        exit()
+        
         if len(seq[1]) > 1024:
             # we don't generate seqs longer than 1024 residues
             print(f"Skipping Seq: {align_name}")
