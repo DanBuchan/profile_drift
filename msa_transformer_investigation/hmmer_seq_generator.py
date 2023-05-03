50be4cdbdd81028ac341b5a62be630df76efd3e6
Daniel Buchan <clone@myself.com>
2023-05-03 18:30:02 +0100

    added hmmemit seq generation

--- msa_transformer_investigation/hmmer_seq_generator.py
+++ msa_transformer_investigation/hmmer_seq_generator.py
@@ -3,7 +3,7 @@ import sys
 from collections import defaultdict
 
 """
-python hmmer_seq_generator.py ../iteration_summary.csv
+python hmmer_seq_generator.py ../iteration_summary.csv ~/Data/pfam/Pfam-A.hmm
 """
 
 def read_drifts(file):
@@ -24,9 +24,35 @@ def make_drift_set_non_redundant(drift_families):
     return(families)
 
 
+def make_hmms_file(drift_set, hmm_file):
+    current_hmm_data = []
+    current_pfam_id = ''
+    current_hmm_name = ''
+    fhOut = open("hmm_subset.hmm", "w")
+    with open(hmm_file, "r") as fhIn:
+        for line in fhIn:
+            current_hmm_data.append(line)
+            if line.startswith("NAME"):
+                current_hmm_name = line[6:]
+            if line.startswith("ACC"):
+                current_pfam_id = line[6:].split(".")[0]
+            if line.startswith("//"):
+                if current_pfam_id in drift_set:
+                    replacement_name = current_hmm_data[1].rstrip()
+                    replacement_name += f"|{current_pfam_id}\n"
+                    current_hmm_data[1] = replacement_name
+                    for output_ln in current_hmm_data:
+                        fhOut.write(output_ln)
+                current_hmm_data = []
+                current_pfam_id = ''
+    fhOut.close()
+
+def runhmmemit():
+    pass
+    #NOT IMPLEMENTED, ran hmmemit on commandline as:
+    # 
+
 drift_families = read_drifts(sys.argv[1])
 nr_drift_set = make_drift_set_non_redundant(drift_families)
-
-# skim through Pfam-A.hmm and retrieve everything in nr_drift_set
-# cat to one file then run
-# ./hmmemit -N 200 ~/Data/pfam/test.hmm
+make_hmms_file(nr_drift_set, sys.argv[2])
+# run_hmmemit()
